201033b115f84d9486bc0bbedc660f6f04e3acd8
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2014-09-02 22:02:19 +0700

    Updated UnicodeFix & Clean.bat

--- Clean.bat
+++ Clean.bat
@@ -2,20 +2,20 @@ RMDIR bin /s /q
 RMDIR Build /s /q
 
 @ECHO off
-CALL :CLEAN_TH "Dialog\"
-CALL :CLEAN_TH "Game\Help\"
-CALL :CLEAN_TH "Game\Scenario\"
+CALL :CLEAN_TH_DIR ".\"
 GOTO :END
 
-:CLEAN_TH
+:CLEAN_TH_DIR
 SET "dir=%~1"
-FOR %%f IN (%dir%*.rsc) DO (CALL ::CLEAN_TH_IF "%dir%" "%%~nf.rsc")
+IF "%dir%"==".\TH\" GOTO :EOF
+FOR /D %%d IN ("%dir%*") DO (CALL :CLEAN_TH_DIR "%dir%%%~nd\")
+FOR %%f IN ("%dir%*.rsc") DO (CALL :CLEAN_TH_IF "%dir%" "%%~nf.rsc")
 GOTO :EOF
 
 :CLEAN_TH_IF
 SET "dir=%~1"
 SET "file=%~2"
-IF EXIST "TH\%dir%%file%" (CALL :CLEAN_TH_DEL "%dir%%file%")
+IF EXIST ".\TH\%dir%%file%" (CALL :CLEAN_TH_DEL "%dir%%file%")
 GOTO :EOF
 
 :CLEAN_TH_DEL
