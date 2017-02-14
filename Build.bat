8d00b43a7e08561620cc70c0c6fdb4ca11988203
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2017-02-15 01:42:00 +0700

    Directory structure change

--- Build.bat
+++ Build.bat
@@ -2,9 +2,9 @@
 FOR %%* IN (.) DO SET "ModDirName=%%~n*"
 
 @ECHO on
-UnicodeFix64.exe TH/ ./
-..\bin\x64\Tools-x64.exe /build Package.rsc:list /pathres ../%ModDirName% /pathdat ../%ModDirName%/bin
-..\bin\x64\Tools-x64.exe /mod Package.rsc:translationThai /pathres ../%ModDirName% /pathdat ../%ModDirName%/bin /pathpkg ../%ModDirName%/
+UnicodeFix64.exe TH/ data/
+..\bin\x64\Tools-x64.exe /build Package.rsc:list /pathres ../%ModDirName%/data /pathdat ../%ModDirName%/data/bin
+..\bin\x64\Tools-x64.exe /mod Package.rsc:translationThai /pathres ../%ModDirName%/data /pathdat ../%ModDirName%/data/bin /pathpkg ../%ModDirName%/data/
 
 @ECHO off
 :: delay hack for win xp
