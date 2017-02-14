f21fe75fa0adaa35ebe902abba03c6a7c726fb91
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2017-02-15 01:57:59 +0700

    Update build script

--- Build.bat
+++ Build.bat
@@ -5,6 +5,7 @@ FOR %%* IN (.) DO SET "ModDirName=%%~n*"
 UnicodeFix64.exe TH/ data/
 ..\bin\x64\Tools-x64.exe /build Package.rsc:list /pathres ../%ModDirName%/data /pathdat ../%ModDirName%/data/bin
 ..\bin\x64\Tools-x64.exe /mod Package.rsc:translationThai /pathres ../%ModDirName%/data /pathdat ../%ModDirName%/data/bin /pathpkg ../%ModDirName%/data/
+XCOPY ..\%ModDirName%\data\translationThai.pkm ..\bin\WinData\ /Y
 
 @ECHO off
 :: delay hack for win xp
